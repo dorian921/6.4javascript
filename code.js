d26b379da3f6bdad0581df9e9b6ad5adf78a95b3
dorian921 <144111488+dorian921@users.noreply.github.com>
2023-10-25 09:37:39 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,24 @@
+document.getElementById(calculator)
+
+function calculator(num, num2) {
+    let answer = num + num2;
+    console.log(answer);
+}
+
+document.getElementById(multiplybyfive)
+
+function multiplybyfive(num, num2) {
+    let answer = num * num2;
+    console.log(answer);
+}
+
+document.getElementById(calculateminutes) 
+
+    function calculateminutes(num, num2) {
+        let answer = num / num2;
+        console.log(answer)
+    
+    
+    }
+
+
